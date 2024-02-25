8d5b8b37525d9712cac6f75725639d46b5c6be54
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2024-02-25 19:57:57 -0300

    c6-Tela de login e funcao de logout (#5)

--- site/frontend/src/components/navbar/Topbar.jsx
+++ site/frontend/src/components/navbar/Topbar.jsx
@@ -4,13 +4,20 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MenuIcon from '@mui/icons-material/Menu';
 import Sidebar from "./Sidebar";
+import { NavLink } from "react-router-dom";
+import LogoutIcon from '@mui/icons-material/Logout';
 
 
 const Topbar = () => {
   const [open, setOpen] = useState(false);
+  const [dropdown, setDropdown] = useState(true);
 
   const handleOpen = () => {
     setOpen(!open);
+  }
+
+  const handleDropdown = () => {
+    setDropdown(!dropdown);
   }
 
   return (
@@ -28,7 +35,7 @@ const Topbar = () => {
             <div className="flex items-center mr-4 hover:text-slate-600 cursor-pointer text-primary">
               <SettingsIcon fontSize="" className="text-3xl" />
             </div>
-            <div className="flex items-center mr-4 cursor-pointer text-primary hover:text-slate-600 ">
+            <div className="flex items-center mr-4 cursor-pointer text-primary hover:text-slate-600 " onClick={() => handleDropdown()}>
               <div className="flex flex-col text-center mr-2">
                 <h2 className="font-bold text-xl mb-0 pb-0 capitalize">Fernando Pereira</h2>
                 <p className="text-secondary italic text-xs	mt-0 pt-0 capitalize">Empresas empresas</p>
@@ -36,6 +43,15 @@ const Topbar = () => {
               <AccountCircleIcon fontSize="" className="text-[45px] " />
             </div>
           </div>
+          <div className={`absolute md:mt-[60px] right-10 bg-[#fff] ${dropdown ? "block" : "hidden"} `}>
+            <ul>
+              <NavLink to="/login" className="cursor-pointer hover:bg-purple-highlight hover:text-amber p-2 w-48 border-2 select-none flex items-center">
+                <LogoutIcon fontSize="" className="text-2xl mr-4" />
+                <span>Profile</span>
+              </NavLink>
+            </ul>
+          </div>
+
         </div>
       </nav >
     </>
--- site/frontend/src/views/LoginPage.jsx
+++ site/frontend/src/views/LoginPage.jsx
@@ -1,9 +1,56 @@
 import React from "react";
+import Logo from "../assets/img/logo.png";
 
 const LoginPage = () => {
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+
+    const data = {
+      email: e.target.email.value,
+      password: e.target.password.value
+    }
+
+    try {
+      console.log(data);
+      window.location.href = "/dashboard";
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <div>
-      <h1 className="text-xl text-red-600">Login Page</h1>
+    <div className="w-screen h-screen bg-[#EEEEEE] flex flex-col md:flex-row">
+      <div className="w-full bg-purple-contrast hidden md:block">
+        <div className="w-full h-full flex items-center justify-center">
+          <img src={Logo} alt="Logo" className="" />
+        </div>
+      </div>
+      <div className="w-full h-full bg-[#EEEEEE] flex items-center  justify-center">
+        <div className=" md:mx-16 px-16 py-24  bg-[#fff] shadow-2xl rounded-3xl w-full">
+          <h1 className="text-4xl font-semibold mb-8">Login</h1>
+          <form className="flex flex-col select-none" onSubmit={onSubmit}>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" className='
+                                    w-full mb-4 p-2 rounded-md border border-gray-300 shadow-md focus:outline-none 
+                                    focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                                    disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                                    invalid:text-pink-600
+                                    focus:invalid:border-pink-500 focus:invalid:ring-pink-500'
+              required />
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" className="
+            w-full mb-16 p-2 rounded-md border border-gray-300 shadow-md focus:outline-none 
+            focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+            invalid:text-pink-600
+            focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+              required
+            />
+            <button className="bg-purple-highlight py-4 px-8 rounded-lg uppercase text-white text-lg font-semibold hover:bg-purple-contrast duration-150 ease-in-out" type="submit">Entrar</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
